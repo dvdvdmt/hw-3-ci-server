3981a9c65e79ef3b8f206c1db2384f908aac006e
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-03-14 13:06:49 +0500

    add build list handler

--- server/api-router.js
+++ server/api-router.js
@@ -7,9 +7,14 @@ const router = express.Router();
 router.get('/settings', ah(async (req, res) => {
   const {data} = await dbApi.get('/conf');
   let resJson = {};
-  if (data.data) {
-    const {repoName, buildCommand, mainBranch, period} = data.data;
-    resJson = {repoName, buildCommand, mainBranch, period};
+  const d = data.data;
+  if (d) {
+    resJson = {
+      repoName: d.repoName,
+      buildCommand: d.buildCommand,
+      mainBranch: d.mainBranch,
+      period: d.period,
+    };
   }
   res.json(resJson);
 }));
@@ -17,6 +22,23 @@ router.get('/settings', ah(async (req, res) => {
 router.post('/settings', ah(async (req, res) => {
   const {data} = await dbApi.post('/conf', req.body);
   res.json(data);
+}));
+
+router.get('/builds', ah(async (req, res) => {
+  const {data} = await dbApi.get('/build/list');
+  let resJson = [];
+  if (data.data && data.data.length) {
+    resJson = data.data.map((el) => ({
+      buildNumber: el.buildNumber,
+      commitHash: el.commitHash,
+      commitMessage: el.commitMessage,
+      authorName: el.authorName,
+      status: el.status,
+      start: el.start,
+      duration: el.duration,
+    }));
+  }
+  res.json(resJson);
 }));
 
 exports.apiRouter = router;
