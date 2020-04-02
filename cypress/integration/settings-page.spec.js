83c7ec4bf3fddecbfb07bed6040f78144f01aaec
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-04-02 07:20:32 +0500

    show settings if they exist

--- cypress/integration/settings-page.spec.js
+++ cypress/integration/settings-page.spec.js
@@ -13,4 +13,21 @@ describe('settings page >', () => {
     cy.get('{mainBranch}').should('have.value', '');
     cy.get('{period}').should('have.value', '0');
   });
+
+  it('shows settings if they exist', () => {
+    const settings = {
+      repoName: 'my-repo-name',
+      buildCommand: 'npm run build',
+      mainBranch: 'master',
+      period: 10,
+    };
+    cy.setSettings(settings);
+    cy.visit('/settings');
+    cy.wait('@fetch-settings');
+    cy.get('{repoName}').should('have.value', settings.repoName);
+    cy.get('{buildCommand}').should('have.value', settings.buildCommand);
+    cy.get('{mainBranch}').should('have.value', settings.mainBranch);
+    cy.get('{period}').should('have.value', `${settings.period}`);
+    cy.deleteSettings();
+  });
 });
--- server/error-handler.js
+++ server/error-handler.js
@@ -1,7 +1,18 @@
 function errorHandler(err, req, res, next) {
   if (err) {
-    console.error(err.stack);
-    res.status(500).send({message: err.message});
+    if (err.isAxiosError && err.response) {
+      console.error(err.response);
+      const {status, data, statusText} = err.response;
+      res.status(status);
+      if (data) {
+        res.send({message: data.title, formError: data.errors});
+      } else {
+        res.send({message: statusText});
+      }
+    } else {
+      console.error(err.stack);
+      res.status(500).send({message: err.message});
+    }
   } else {
     next();
   }
