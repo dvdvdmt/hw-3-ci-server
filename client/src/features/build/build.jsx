6b8e001d259adfbe0730842939015c4e48505ed3
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-04-12 17:03:41 +0500

    move build card to separate component

---
+++ client/src/features/build/build.jsx
@@ -0,0 +1,56 @@
+import cn from 'classnames';
+import React from 'react';
+import {
+  CalendarIcon,
+  ClockIcon,
+  CommitIcon,
+  DoneIcon,
+  FailIcon,
+  StopwatchIcon,
+  UserIcon,
+} from '../../components/icons';
+import './build.scss';
+
+export function Build({className, build}) {
+  const buildStatus = build.status.toLowerCase();
+  return (
+    <a href="#/build/id" className={cn(className, 'Build', `Build_status_${buildStatus}`)}>
+      <div className="Build-Status">
+        <DoneIcon width="16" height="16" className="Build-StatusIcon Build-StatusIcon_success" />
+        <FailIcon width="16" height="16" className="Build-StatusIcon Build-StatusIcon_fail" />
+        <ClockIcon width="16" height="16" className="Build-StatusIcon Build-StatusIcon_running" />
+      </div>
+      <div className="Build-Info">
+        <div className="Build-Header">
+          <div className="Build-HeaderFirst">
+            <div className="Build-Number">#{build.buildNumber}</div>
+            <div className="Build-CommitMessage">{build.commitMessage}</div>
+          </div>
+          <div className="Build-HeaderSecond">
+            <div className="Build-Commit">
+              <div className="Build-CommitBranch">
+                <CommitIcon width="16" height="16" />
+                <span className="Build-CommitBranchName">{build.branchName}</span>
+              </div>
+              <div className="Build-CommitHash">{build.commitHash}</div>
+            </div>
+            <div className="Build-Author">
+              <UserIcon width="16" height="16" />
+              <span className="Build-AuthorName">{build.authorName}</span>
+            </div>
+          </div>
+        </div>
+        <div className="Build-Times">
+          <div className="Build-DateTime">
+            <CalendarIcon width="16" height="16" />
+            <span className="Build-DateTimeText">{build.start}</span>
+          </div>
+          <div className="Build-Duration">
+            <StopwatchIcon width="16" height="16" />
+            <span className="Build-DurationText">{build.duration}</span>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+}
