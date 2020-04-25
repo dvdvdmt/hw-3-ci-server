d68c59ce76cd562e1f46d9fba745dceb81f290e0
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-04-25 10:19:13 +0500

    add error catching on agent registering

--- build-agent/build-agent.js
+++ build-agent/build-agent.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const {serverApi} = require('./server-api.js');
 
-serverApi.fetchAgentSettings().then(({data: settings}) => {
-  const {port} = settings;
-  const app = express();
+serverApi
+  .fetchAgentSettings()
+  .then(({data: settings}) => {
+    const {port} = settings;
+    const app = express();
 
-  app.listen(port, async () => {
-    console.log(`Build agent http://localhost:${port}`);
+    app.listen(port, async () => {
+      console.log(`Build agent is listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Can't register build agent`);
+    console.log(error);
   });
-});
